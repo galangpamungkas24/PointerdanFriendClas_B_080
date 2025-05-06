e42665b8dba30b6a45eabb2ff1e82882d52dedaf
galang <ahmad.galang.pamungkas24@gmail.com>
2025-05-06 15:48:48 +0700

    friendclass1.1

--- PointerDua.cpp
+++ PointerDua.cpp
@@ -19,3 +19,77 @@ int main()
     delete mhs;
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang 
+{
+private:
+    string nama;
+public:
+     void setNama(string pNama);
+      friend class siswa; 
+};
+
+class siswa 
+{
+private:
+    int id;
+public:
+    void setId(int pId);
+    void displayAll(orang &a); 
+};
+
+void siswa::displayAll(orang &a) 
+{
+    cout << id << endl << a.nama ;
+}
+void orang::setNama(string pNama) 
+{
+    nama = pNama;
+}
+void siswa::setId(int pId) 
+{
+    id = pId;
+}
