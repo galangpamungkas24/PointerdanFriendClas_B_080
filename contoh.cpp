dff4ace6bcd25d4a6e9c2fbda75c814e0e2d5b4c
galang <ahmad.galang.pamungkas24@gmail.com>
2025-05-06 15:06:55 +0700

    first commit

---
+++ contoh.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+    public: // akses modifier
+    int panjang, lebar;
+
+    public:
+
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+
+    Lingkaran ol;
+
+};
+
+class Lingkaran
+{
+    public:
+    int jarijari;
+
+    void input()
+    {
+    cout << "Masukkan jari-jari: ";
+    cin >> jarijari; 
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
+};
