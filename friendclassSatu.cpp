46bf069855a0be0ec59f6d1e590442bb22cc550b
galang <ahmad.galang.pamungkas24@gmail.com>
2025-05-06 15:57:50 +0700

    friendfunction1.2

--- friendclassSatu.cpp
+++ friendclassSatu.cpp
@@ -54,15 +54,7 @@ void siswa::setId(int pId)
 
 
 
-#include <iostream>
-using namespace std;
 
-class pelajar;
-
-class manusia {
-public:
-    void showNilaiPelajar(pelajar &x);
-};
 
 class pelajar {
 private:
