23a1552e2506df5d1205cefe3deb3bdad5cf3cbe
galang <ahmad.galang.pamungkas24@gmail.com>
2025-05-06 15:39:08 +0700

    pointer1

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 class mahasiswa{
-    
+
     public:
     int nim;
     void ShowNim(){
         cout<<"No Induk = "<<nim<<endl;
     }
+
 };
 
 int main (){
     mahasiswa mhs{1}; //Object mhs
     mhs.ShowNim(); //Member Acces Operator
+
 
     mahasiswa &refMhs = mhs; //Pointer Reference refMhs
     refMhs.nim = 2; //Member Access Operator
